74749b7d61bdcb09ad9b70cf1e45f5b4bda64777
Thomas Grill <gr@grrrr.org>
2025-12-29 17:03:14 +0100

    Generalized AudioMixer_F32 with templates

--- AudioMixer_F32.h
+++ AudioMixer_F32.h
@@ -20,50 +20,73 @@
 #include <arm_math.h> 
 #include <AudioStream_F32.h>
 
-class AudioMixer4_F32 : public AudioStream_F32 {
-//GUI: inputs:4, outputs:1  //this line used for automatic generation of GUI node
-//GUI: shortName:Mixer4
+template <int inputs>
+class AudioMixer_F32 : public AudioStream_F32 {
 public:
-    AudioMixer4_F32() : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
-	AudioMixer4_F32(const AudioSettings_F32 &settings) : AudioStream_F32(4, inputQueueArray) { setDefaultValues(); }
+  AudioMixer_F32() : AudioStream_F32(inputs, inputQueueArray) { setDefaultValues(); }
+	AudioMixer_F32(const AudioSettings_F32 &settings) : AudioStream_F32(inputs, inputQueueArray) { setDefaultValues(); }
 	
 	void setDefaultValues(void) {
-		for (int i=0; i<4; i++) multiplier[i] = 1.0;
+		for (int i=0; i<inputs; i++) multiplier[i] = 1.0;
 	}
 	
-    virtual void update(void);
+  virtual void update(void) {
+    audio_block_f32_t *in, *out=NULL;
 
-    void gain(unsigned int channel, float gain) {
-      if (channel >= 4 || channel < 0) return;
-      multiplier[channel] = gain;
+    //get the first available channel
+    int channel = 0;
+    while  (channel < inputs) {
+      out = receiveWritable_f32(channel);
+      if (out) break;
+      channel++;
     }
+    if (!out) return;  //there was no data output array.  so exit.
 
-  private:
-    audio_block_f32_t *inputQueueArray[4];
-    float multiplier[4];
+    if(!multiplier[channel])
+      arm_fill_f32(0, out->data, out->length);
+    else if(multiplier[channel] != 1)
+      arm_scale_f32(out->data, multiplier[channel], out->data, out->length);
+
+    //add in the remaining channels, as available
+    for(channel++; channel < inputs; channel++) {
+      if(multiplier[channel]) {
+        in = receiveReadOnly_f32(channel);
+        if (in) {
+          if(multiplier[channel] == 1)
+            arm_add_f32(out->data, in->data, out->data, out->length);
+          else {
+            audio_block_f32_t *tmp = allocate_f32();
+              arm_scale_f32(in->data, multiplier[channel], tmp->data, tmp->length);
+            arm_add_f32(out->data, tmp->data, out->data, tmp->length);
+            AudioStream_F32::release(tmp);
+          }
+
+          AudioStream_F32::release(in);
+        } else {
+          //do nothing, this vector is empty
+        }
+      }
+    }
+    AudioStream_F32::transmit(out);
+    AudioStream_F32::release(out);
+  }
+
+  void gain(unsigned int channel, float gain) {
+    if (channel >= inputs || channel < 0) return;
+    multiplier[channel] = gain;
+  }
+
+private:
+  audio_block_f32_t *inputQueueArray[inputs];
+  float multiplier[inputs];
 };
 
-class AudioMixer8_F32 : public AudioStream_F32 {
+using AudioMixer4_F32 = AudioMixer_F32<4>;
+//GUI: inputs:4, outputs:1  //this line used for automatic generation of GUI node
+//GUI: shortName:Mixer4
+
+using AudioMixer8_F32 = AudioMixer_F32<8>;
 //GUI: inputs:8, outputs:1  //this line used for automatic generation of GUI node
 //GUI: shortName:Mixer8
-public:
-    AudioMixer8_F32() : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
-    AudioMixer8_F32(const AudioSettings_F32 &settings) : AudioStream_F32(8, inputQueueArray) { setDefaultValues();}
-	
-	void setDefaultValues(void) {
-      for (int i=0; i<8; i++) multiplier[i] = 1.0;
-    }
-
-    virtual void update(void);
-
-    void gain(unsigned int channel, float gain) {
-      if (channel >= 8 || channel < 0) return;
-      multiplier[channel] = gain;
-    }
-
-  private:
-    audio_block_f32_t *inputQueueArray[8];
-    float multiplier[8];
-};
 
 #endif
